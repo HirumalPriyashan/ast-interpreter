c1682ecd7453ef4bed4148ce775f5bd848f9b07c
HirumalPriyashan <hirupriyashanrc@gmail.com>
2021-10-28 13:24:27 +0530

    add CSEStandardizer

---
+++ src/CSEMachine/CSEStandardizer.java
@@ -0,0 +1,11 @@
+package CSEMachine;
+
+import Node.Node;
+import Standardizer.Standardizer;
+
+public class CSEStandardizer implements Standardizer {
+    @Override
+    public void standardize(Node node) {
+
+    }
+}
