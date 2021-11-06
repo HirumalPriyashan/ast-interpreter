981c0be52cd27f9b2c7a853345a16bc0f88ad618
HirumalPriyashan <hirupriyashanrc@gmail.com>
2021-11-06 23:32:06 +0530

    add comments for node

--- src/Node/Node.java
+++ src/Node/Node.java
@@ -9,6 +9,13 @@ import java.util.List;
 
 import Logger.Logger;
 
+/**
+ * Class representation for node in syntax tree
+ * 
+ * @author Hirumal Priyshan
+ * @version 1.0
+ * @since 1.0
+ */
 public class Node implements IVisitable {
     private String token;
     private List<Node> children;
@@ -16,6 +23,11 @@ public class Node implements IVisitable {
     private int depth;
     private boolean isStandardized;
 
+    /**
+     * Class constructor.
+     * 
+     * @param token token for the node
+     */
     public Node(String token) {
         this.token = token;
         this.children = new ArrayList<Node>();
@@ -24,18 +36,38 @@ public class Node implements IVisitable {
         this.isStandardized = false;
     }
 
+    /**
+     * Getter for token
+     * 
+     * @return token in the node
+     */
     public String getToken() {
         return this.token;
     }
 
+    /**
+     * Getter for token
+     * 
+     * @param token token to set for symbol
+     */
     public void setToken(String token) {
         this.token = token;
     }
 
+    /**
+     * Getter for children
+     * 
+     * @return children list for the node
+     */
     public List<Node> getChildren() {
         return this.children;
     }
 
+    /**
+     * Setter for children
+     * 
+     * @param children list of children
+     */
     public void setChildren(List<Node> children) {
         this.removeChildren();
         for (Node node : children) {
@@ -43,6 +75,11 @@ public class Node implements IVisitable {
         }
     }
 
+    /**
+     * Setter for children with editing their depths
+     * 
+     * @param children list of children
+     */
     public void setChildrenWithDepth(List<Node> children) {
         this.removeChildren();
         for (Node node : children) {
@@ -50,38 +87,76 @@ public class Node implements IVisitable {
         }
     }
 
+    /**
+     * Add a child to the children list
+     * 
+     * @param child child to be add
+     */
     public void addChild(Node child) {
         child.setParent(this);
         this.children.add(child);
     }
 
+    /**
+     * Add a child with changing thire depth to the children list
+     * 
+     * @param child child to be add
+     */
     public void addChildWithDepth(Node child) {
         child.setParent(this);
         child.increaseDepthBy(this.getDepth() - child.getDepth() + 1);
         this.children.add(child);
     }
 
+    /**
+     * Remove all children from children list
+     */
     public void removeChildren() {
         this.children = new ArrayList<Node>();
         return;
     }
 
+    /**
+     * Getter for parent
+     * 
+     * @return parent node of the node
+     */
     public Node getParent() {
         return this.parent;
     }
 
+    /**
+     * Setter for parent
+     * 
+     * @param parent node to be set as parent
+     */
     public void setParent(Node parent) {
         this.parent = parent;
     }
 
+    /**
+     * Getter for depth
+     * 
+     * @return the depth of the node
+     */
     public int getDepth() {
         return this.depth;
     }
 
+    /**
+     * Setter for depth
+     * 
+     * @param depth depth to set for the node
+     */
     public void setDepth(int depth) {
         this.depth = depth;
     }
 
+    /**
+     * increase the depth of node by given marging
+     * 
+     * @param margin size to increase or decrease the depth
+     */
     public void increaseDepthBy(int margin) {
         this.depth += margin;
         for (Node node : children) {
@@ -89,6 +164,13 @@ public class Node implements IVisitable {
         }
     }
 
+    /**
+     * Getter for isStandardized for node
+     * isStandardized will have value of <b>true</b> if the node has 
+     * been standardized
+     * 
+     * @return value of isStandardized
+     */
     public boolean getIsStandardized() {
         if (this.children.size() == 0){
             this.isStandardized = true;
@@ -96,10 +178,21 @@ public class Node implements IVisitable {
         return this.isStandardized;
     }
 
+    /**
+     * Setter for isStandardized
+     * 
+     * @param isStandardized value to set for isStandardized
+     */
     public void setIsStandardized(boolean isStandardized) {
         this.isStandardized = isStandardized;
     }
 
+    /**
+     * Returns the next node to append the new incoming node by depth
+     * 
+     * @param depth depth of the incoming node
+     * @return next node which should be the parent of new node
+     */
     public Node getNextToAppend(int depth){
         if (depth == this.depth + 1) {
             return this;
@@ -107,11 +200,9 @@ public class Node implements IVisitable {
         return this.children.get(this.children.size() - 1).getNextToAppend(depth);
     }
 
-    @Override
-    public String toString() {
-        return ".".repeat(this.depth * 1) + this.token ;
-    }
-
+    /**
+     * Prings the structure for the node with its successors
+     */
     public void printNode() {
         Logger.log(this);
         for (Node node : this.children) {
@@ -119,12 +210,9 @@ public class Node implements IVisitable {
         }
     }
 
-    public List<String> preOrderTraverse(List<String> nodes) {
-        nodes.add(this.getToken());
-        for (Node node : this.children) {
-            nodes = node.preOrderTraverse(nodes);
-        }
-        return nodes;
+    @Override
+    public String toString() {
+        return ".".repeat(this.depth * 1) + this.token ;
     }
 
     @Override
